ee711bf71d15201d4d9bcebb8565b2d4586c2a37
zadigg <62830716+zadigg@users.noreply.github.com>
2021-09-30 18:45:28 +0300

    final

--- components/AfterLogin.js
+++ components/AfterLogin.js
@@ -5,6 +5,7 @@ function AfterLogin({ requests }) {
     <div className="px-5 my-10 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6  flex-wrap justify-center">
       {requests.map((result) => (
         <Thumbnails key={result.id} result={result} />
+        // chceck
       ))}
     </div>
   );
